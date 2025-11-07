c5780b0eaa31275b7c405e8bc8ed2f3295f1dec3
NATALIIAMN <162594144+NATALIIAMN@users.noreply.github.com>
2025-11-07 21:52:18 +0200

    Update

--- ProgramCodeModified.py
+++ ProgramCodeModified.py
@@ -1,10 +1,10 @@
-def main():
+def print_product_name():
     product_name = (
         "Вебсервіс для проведення відеоконференцій та "
-        "реалізації моніторингу робочого місця"
+        "моніторингу робочого місця"
     )
     print(product_name)
 
 
 if __name__ == "__main__":
-    main()
+    print_product_name()
