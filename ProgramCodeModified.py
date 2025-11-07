c734d8716ae9ff578104b2fcf71e115ea1f03b24
NATALIIAMN <162594144+NATALIIAMN@users.noreply.github.com>
2025-11-07 21:56:21 +0200

    Update

--- ProgramCodeModified.py
+++ ProgramCodeModified.py
@@ -1,4 +1,13 @@
+"""
+This module prints the name of the software product:
+'Вебсервіс для проведення відеоконференцій та моніторингу робочого місця'.
+"""
+
+
 def print_product_name():
+    """
+    Prints the product name to the terminal.
+    """
     product_name = (
         "Вебсервіс для проведення відеоконференцій та "
         "моніторингу робочого місця"
